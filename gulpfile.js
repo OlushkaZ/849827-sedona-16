8c745b47b75a10e2a7d841e3899e9da0ae1c84e8
OlushkaZ <gincgo@yandex.ru>
2019-04-27 23:29:50 +0300

    build2

--- gulpfile.js
+++ gulpfile.js
@@ -55,13 +55,13 @@ gulp.task("imagemin", function(){
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
   ]))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("webp", function(){
   return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("sprite", function(){
@@ -100,5 +100,5 @@ gulp.task("refresh", function(done){
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "webp", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
