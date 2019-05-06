0b1a4fea71b738f6943d1d853fe563966a302fd4
OlushkaZ <gincgo@yandex.ru>
2019-05-06 16:34:57 +0300

    popups mobile

--- source/js/external.js
+++ source/js/external.js
@@ -1,5 +1,13 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var reviewForm = document.querySelector(".review__form");
+var popapSuccess = document.querySelector(".popap-success");
+var popapFailure = document.querySelector(".popap-failure");
+var close = popapSuccess.querySelector(".modal__close");
+var userName = popupFeedback.querySelector("[id=contacts-name]");
+var userFamily = popupFeedback.querySelector("[id=contacts-family]");
+var userEmail = popupFeedback.querySelector("[id=contacts-email]");
+var userTel = popupFeedback.querySelector("[id=contacts-tel]");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,6 +18,26 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+reviewForm.addEventListener("submit", function(evt) {
+  if (!userName.value || !userFamily.value || !userEmail.value || !userTel.value) {
+    evt.preventDefault();
+  popapFailure.classList.add("modal--show");
+  //   // popupFeedback.classList.remove("modal-error");
+  //   // popupFeedback.offsetWidth = popupFeedback.offsetWidth;
+  //   // popupFeedback.classList.add("modal-error");
+  //   if (!userName.value) {
+  //     userName.focus();
+  //   } else if (!userEmail.value) {
+  //     userEmail.focus();
+  //   } else if (!userTel.value) {
+  //     userTel.focus();
+  //   }
+  }
+  else {
+    popapSuccess.classList.add("modal--show");
   }
 });
 
